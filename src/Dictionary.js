f15a7feba2ffa45ad4d85de2ddd4b33dd8727f5e
m-damas <145788515+m-damas@users.noreply.github.com>
2024-03-23 17:51:06 -0500

    Create Synonym component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,7 +14,6 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
 
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=td6b219f3d3oca374173c4ae55621b00`;
     axios.get(apiUrl).then(handleRepsonse);
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import Synonyms from "./Synonyms";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -8,11 +8,16 @@ export default function Meaning(props) {
     <div className="Meaning">
       <br />
       <h3>{props.meaning.partOfSpeech}</h3>
-      <h4>Definition: {props.meaning.definition}</h4>
+      <h4>
+        {" "}
+        <strong>Definition: </strong>
+        {props.meaning.definition}
+      </h4>
       <h4>
         <em>Example: {props.meaning.example}</em>
       </h4>
+
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
-//<Synonyms synonyms={props.meaning.synonyms} />
